00ba1bb25489746f854c04df528353e23d1742e7
secretpray <a.p.ruban@gmail.com>
2022-02-28 14:11:58 +0200

    Turbo flash (notice and alert) TODO

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,21 +1,17 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: %i[ show edit update destroy change_status ]
 
-  # GET /todos or /todos.json
   def index
-    @todos = Todo.where(status: params[:status].presence || 'incomplete')
+    @status = params[:status].presence || 'incomplete'
+    @todos = Todo.where(status: @status )
   end
 
-  # GET /todos/1 or /todos/1.json
-  def show
-  end
+  def show; end
 
-  # GET /todos/new
   def new
     @todo = Todo.new
   end
 
-  # GET /todos/1/edit
   def edit
   end
 
@@ -28,10 +24,8 @@ class TodosController < ApplicationController
         format.turbo_stream
         format.html { redirect_to root_url, notice: "Todo was successfully created." }
       else
-        format.turbo_stream {
-          render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@todo)}_form",
-                                                    partial: "form",
-                                                    locals: { todo: @todo }) }
+        flash.now[:alert] = @todo.errors.full_messages.join('; ')
+        format.turbo_stream
         format.html { render :new, status: :unprocessable_entity }
       end
     end
@@ -40,13 +34,12 @@ class TodosController < ApplicationController
   def update
     respond_to do |format|
       if @todo.update(todo_params)
+        flash.now[:notice] = "Todo was successfully updated."
         format.turbo_stream
         format.html { redirect_to todo_url(@todo), notice: "Todo was successfully updated." }
       else
-        format.turbo_stream {
-          render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@todo)}_form",
-                                                    partial: "form",
-                                                    locals: { todo: @todo }) }
+        flash.now[:alert] = @todo.errors.full_messages.join('; ')
+        format.turbo_stream
         format.html { render :edit, status: :unprocessable_entity }
       end
     end
@@ -56,6 +49,7 @@ class TodosController < ApplicationController
     @todo.destroy
 
     respond_to do |format|
+      flash.now[:notice] = "Todo was successfully destroyed."
       format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@todo)}_container") }
       format.html { redirect_to todos_url, notice: "Todo was successfully destroyed." }
     end
@@ -65,18 +59,18 @@ class TodosController < ApplicationController
     @todo.update(status: todo_params[:status])
 
     respond_to do |format|
+      flash.now[:notice] = "Updated todo status."
       format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@todo)}_container") }
       format.html { redirect_to todos_path, notice: "Updated todo status." }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_todo
       @todo = Todo.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def todo_params
       params.require(:todo).permit(:name, :status)
     end
