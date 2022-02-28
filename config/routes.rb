9f2331d9f1dcb28fbf0e6a127ba65174ef816c6f
secretpray <a.p.ruban@gmail.com>
2022-02-28 16:23:54 +0200

    Added User Model, Columns to Users, Sign Up Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   resources :todos do
     patch :change_status, on: :member
   end
 
-  root "todos#index"
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
+
+  root "static_pages#home"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_27_094018) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_28_135813) do
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "confirmed_at"
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
