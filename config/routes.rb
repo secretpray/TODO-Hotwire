968348421f3229f6db156ed87883124ae41eaab9
secretpray <a.p.ruban@gmail.com>
2022-02-28 16:37:44 +0200

    Create Confirmation Pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   post "sign_up", to: "users#create"
   get "sign_up", to: "users#new"
 
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
+
   root "static_pages#home"
 end
