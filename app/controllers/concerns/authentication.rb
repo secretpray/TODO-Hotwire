67c66de07f34d75ffdf59b7168cf3ba1295dc495
secretpray <a.p.ruban@gmail.com>
2022-02-28 19:08:30 +0200

    Add Friendly Redirects

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -8,6 +8,7 @@ module Authentication
   end
 
   def authenticate_user!
+    store_location
     redirect_to login_path, alert: "You need to login to access that page." unless user_signed_in?
   end
 
@@ -48,4 +49,7 @@ module Authentication
     Current.user.present?
   end
 
+  def store_location
+    session[:user_return_to] = request.original_url if request.get? && request.local?
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,10 @@ class SessionsController < ApplicationController
       if @user.unconfirmed?
         redirect_to new_confirmation_path, alert: "Incorrect email or password."
       elsif @user.authenticate(params[:user][:password])
+        after_login_path = session[:user_return_to] || root_path
         login @user
         remember(@user) if params[:user][:remember_me] == "1"
+        redirect_to after_login_path, notice: "Signed in."
       else
         flash.now[:alert] = "Incorrect email or password."
         render :new, status: :unprocessable_entity
